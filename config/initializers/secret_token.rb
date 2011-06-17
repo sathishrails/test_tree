7e466992b841e0142a9af66e775ea95b59e1b43d
sathishrails <sathitha@sify.com>
2011-06-18 02:13:42 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestTree1::Application.config.secret_token = '1a4b82644de1ef65e34d63bee780da274563963c090854ea705d7ed69e43178ddbdf52688321f127c77d8bd7fb0302be3e12cfda00c57e75f7d5eae27fe22ac7'
